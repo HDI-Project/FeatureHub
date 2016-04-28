9f56be449ae5e6d53661741eacdb8b6e0111b706
csala <c.sala.stq@gmail.com>
2016-04-28 18:52:38 +0200

    Cleanup

--- src/problems/__init__.py
+++ src/problems/__init__.py
@@ -1,29 +1,11 @@
 import imp
 import sys
 
-sys.path.insert(1, '..')
-
 from factory import Session
-
-# >>> import imp
-# >>> foo = imp.new_module("foo")
-# >>> foo_code = """
-# ... class Foo:
-# ...     pass
-# ... """
-# >>> exec foo_code in foo.__dict__
-# >>> foo.Foo.__module__
-# 'foo'
-# >>>
-
-def get_problem_names():
-    # Imported here to avoid having it visible at Module level
-    from orm.admin import Commands
-    admin_commands = Commands()
-    return admin_commands.get_problems()
+from orm.admin import Commands
 
 
-for problem in get_problem_names():
+for problem in Commands().get_problems():
     # Crete a session for each problem and make it importable
     commands = Session(problem)
     module = imp.new_module(problem)
