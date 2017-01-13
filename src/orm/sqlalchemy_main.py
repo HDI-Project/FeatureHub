048ee4d8bcb6f4cc84fbc06db81a9ad66016c7f9
rwedge <roy.wedge@gmail.com>
2017-01-13 15:30:46 -0500

    orm host can be specified by a config file

--- src/orm/sqlalchemy_main.py
+++ src/orm/sqlalchemy_main.py
@@ -1,12 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from orm.sqlalchemy_declarative import Base
-
+from configparser import RawConfigParser
+import os.path
 
 class ORMManager:
     """Initialize the sqlalchemy ORM engine and starts a database session."""
 
-    def __init__(self, database, user='root', password='', host='localhost'):
+    def __init__(self, database, user='root', password='', host=None):
+        if not host:
+             try:
+                config = RawConfigParser()
+                config.read('/etc/featurefactory/featurefactory.conf')
+                host = config.get('mysql','host')
+             except IOError:
+                host = 'localhost'
         conn_string = 'mysql+mysqlconnector://{}:{}@{}/{}'.format(user, password, host, database)
         # print("Opening a new db connection: {}".format(conn_string))
         self.engine = create_engine(conn_string)
