0e782d7f7583056ab6ea59382ff436b324686fed
Micah Smith <micahjsmith@gmail.com>
2017-01-26 01:43:52 +0000

    Generate mysql root password

--- deploy/install_mysql.sh
+++ deploy/install_mysql.sh
@@ -46,11 +46,14 @@ if [ "$(docker ps -a -f name=^/${MYSQL_CONTAINER_NAME}$ | wc -l)" != "2" ]; then
 fi
 
 # Create a mysql container, initialize the featurefactory database, and create
-# an admin user account.
+# an admin user account. Also create a random root password.
+root_password=$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 8 | head -n 1)
+echo $root_password
 ${docker_sudo} docker run \
     --name "$MYSQL_CONTAINER_NAME" \
-    -e MYSQL_ONETIME_PASSWORD \
+    -e MYSQL_ROOT_PASSWORD=$root_password \
     -e "MYSQL_DATABASE=$MYSQL_DATABASE_NAME" \
     -e "MYSQL_USER=$MYSQL_ADMIN_USERNAME" \
     -e "MYSQL_PASSWORD=$MYSQL_ADMIN_PASSWORD" \
+    -d \
     mysql
