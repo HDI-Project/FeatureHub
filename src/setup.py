61405801549b3872f75f8d7571c462aca5776730
Micah Smith <micahjsmith@gmail.com>
2017-03-20 22:26:51 +0000

    Refactor to new package structure

--- src/setup.py
+++ src/setup.py
@@ -1,21 +1,25 @@
-#!/usr/bin/env python
+"""
+User-facing API for the Feature Factory collaborative platform.
+"""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='featurefactory',
-      version='0.1',
-      description='Feature Factory',
-      packages=['orm', 'factory', 'problems'],
-      install_requires=[
-          'jupyter',
-          'matplotlib',
-          'numpy',
-          'mysqlclient',
-          'mysql-connector-python-rf',
-          'pandas',
-          'scikit-learn',
-          'scipy',
-          'sqlalchemy',
-          'sqlalchemy_utils'
-      ]
-     )
+setup(
+    name='featurefactory',
+    version='0.1.0',
+    description='Feature Factory',
+    url='https://github.com/HDI-Project/FeatureFactory',
+    packages=find_packages(),
+    install_requires=[
+        'jupyter',
+        'matplotlib',
+        'numpy',
+        'mysqlclient',
+        'mysql-connector-python-rf',
+        'pandas',
+        'scikit-learn',
+        'scipy',
+        'sqlalchemy',
+        'sqlalchemy_utils',
+    ],
+ )
