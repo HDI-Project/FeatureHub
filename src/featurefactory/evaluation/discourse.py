5a1157faba4b6d7f314fcf0193d18c31fb5ab2f9
Micah Smith <micahjsmith@gmail.com>
2017-05-16 19:58:37 -0400

    WIP: improve discourse posting

--- src/featurefactory/evaluation/discourse.py
+++ src/featurefactory/evaluation/discourse.py
@@ -1,75 +1,113 @@
 import os
 import textwrap
 from pydiscourse import DiscourseClient
+from pydiscourse.exceptions import DiscourseClientError
+
+class DiscourseFeatureTopicTemplate(object):
+    indent = "    "
+    bullet = " * "
+
+    def __init__(self):
+        self.title = "[New Feature] {title}"
+        self.header = "## A new feature was submitted!\n\n"
+        self.problem_name = "* _Problem name_: {problem_name}\n"
+        self.feature_description = "* _Feature description_: {feature_description}\n"
+        self.feature_code = "* _Feature code_:\n\n{feature_code}\n\n"
+        self.feature_metrics = "* _Feature metrics_:\n{feature_metrics}\n\n"
+        self.prompt = ("What do you think? What do you like about this "
+                       "feature? How could it be improved? Leave your "
+                       "comments below, or get to work with your ideas!\n\n")
+        self.author = ("----------\n\n"
+                      "(submitted by user <em>{user_name}</em>)\n")
+
+    def render(self, problem_name, feature_description, feature_code,
+            feature_metrics, user_name):
+        result = ""
+
+        result += self.header
+        result += self.problem_name.format(
+                problem_name=problem_name)
+        result += self.feature_description.format(
+                feature_description=feature_description)
+        result += self.feature_code.format(
+                feature_code=feature_code)
+        result += self.prompt
+        result += self.author.format(
+                user_name=user_name)
+
+        return result
+
+    def render_title(self, title):
+        return self.title.format(title=title)
+
+class DiscourseFeatureTopic(object):
+
+    def __init__(self, feature, metrics):
+        self.feature = feature
+        self.metrics = metrics
+
+        client = DiscourseClient(
+                host="https://{}".format(os.environ.get("DISCOURSE_DOMAIN_NAME")),
+                api_username=os.environ.get("DISCOURSE_CLIENT_API_USERNAME"),
+                api_key=os.environ.get("DISCOURSE_CLIENT_API_TOKEN"))
+        self.client = client
 
 
-def post_feature(feature, metrics):
-    client = DiscourseClient(
-            host="https://{}".format(os.environ.get("DISCOURSE_DOMAIN_NAME")),
-            api_username=os.environ.get("DISCOURSE_CLIENT_API_USERNAME"),
-            api_key=os.environ.get("DISCOURSE_CLIENT_API_TOKEN"))
+    def format_code(self):
+        code = self.feature.code
+        indent = DiscourseFeatureTopicTemplate.indent
+        tmp = []
+        tmp.append(indent + "```")
+        for line in code.split("\n"):
+            tmp.append(indent + line)
+        tmp.append(indent + "```")
+        feature_code = "\n".join(tmp)
+        return feature_code
 
-    params = [
-        feature.problem.name,
-        feature.description,
-        feature.code,
-        metrics,
-        feature.user.name,
-    ]
-    content = _render_feature_post_template(*params)
+    def format_metrics(self):
+        metrics = self.metrics
+        bullet = DiscourseFeatureTopicTemplate.bullet
+        tmp = []
+        for metric in metrics:
+            d = metric.convert(kind="db")
+            tmp.append(bullet + "{}: {}".format(d["name"], d["value"]))
+        feature_metrics = "\n".join(tmp)
+        return feature_metrics
 
-    result = client.create_post(
-            category=os.environ.get("DISCOURSE_FEATURE_CATEGORY_NAME"),
-            title="[New Feature] {}".format(feature.description),
-            content=content)
+    def get_params(self):
+        params = [
+            self.feature.problem.name,
+            self.feature.description,
+            self.format_code(),
+            self.format_metrics(),
+            feature.user.name,
+        ]
+        return params
 
-    url = "https://{}/t/{}".format(
-            os.environ.get("DISCOURSE_DOMAIN_NAME"),
-            result["topic_slug"])
+    def _escape_user_name(name):
+        """Replace `_` in name with `&lowbar;`."""
+        return name.replace("_", "&lowbar;")
 
-    # return the url of the new post
-    # result = ""
-    return url
+    def post_feature(self):
+        params = self.get_params()
+        content = DiscourseFeatureTopicTemplate().render(*params)
+        try:
+            post = client.create_post(
+                    category=os.environ.get("DISCOURSE_FEATURE_CATEGORY_NAME"),
+                    title=.format(feature.description),
+                    content=content)
+        except Discourse:
 
-_template = \
-"""
-## A new feature was submitted!
-
-* _Problem name_: {problem_name}
-* _Feature description_: {feature_description}
-* _Feature code_:
-
-{feature_code}
-
-* _Feature metrics_:
-{feature_metrics}
-
-What do you think? What do you like about this feature? How could it be improved? Leave your comments below, or get to work with your ideas!
-
-----------
-
-(submitted by user <em>{user_name}</em>)
-"""
+        # return the url of the new post
+        url = "https://{}/t/{}".format(
+                os.environ.get("DISCOURSE_DOMAIN_NAME"),
+                post["topic_slug"])
+        return url
 
 def _render_feature_post_template(problem_name, feature_description,
         code, metrics, user_name):
 
-    # format a couple objects
-    indent = "    "
-    bullet = " * "
 
-    tmp = []
-    tmp.append(indent + "```")
-    for line in code.split("\n"):
-        tmp.append(indent + line)
-    tmp.append(indent + "```")
-    feature_code = "\n".join(tmp)
-
-    tmp = []
-    for metric in metrics:
-        d = metric.convert(kind="db")
-        tmp.append(bullet + "{}: {}".format(d["name"], d["value"]))
-    feature_metrics = "\n".join(tmp)
 
     # user_name = _escape_user_name(user_name)
 
@@ -79,6 +117,3 @@ def _render_feature_post_template(problem_name, feature_description,
             feature_code=feature_code,
             feature_metrics=feature_metrics,
             user_name=user_name)
-
-def _escape_user_name(name):
-    return name.replace("_", "&lowbar;")
