eaf598f018c906290aa86886ccdfa0a0805d1bef
Micah Smith <micahjsmith@gmail.com>
2017-05-31 12:50:42 +0000

    Add admin notebook launch and config

---
+++ deploy/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.certfile = "/etc/letsencrypt/cert.pem"
+c.NotebookApp.keyfile = "/etc/letsencrypt/privkey.pem"
+c.NotebookApp.ip = "*"
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+c.NotebookApp.notebook_dir = "/home/jovyan/notebooks"
---
+++ deploy/launch_admin_notebook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source .env
+source .env.local >/dev/null 2>&1 || true
+
+docker run -d \
+    --name jpy-admin \
+    -v "${FF_DATA_DIR}/data:/data:rw" \
+    -v "${FF_DATA_DIR}/users/admin/:/home/jovyan:rw" \
+    -v "${SECRETS_VOLUME_NAME}:/etc/letsencrypt:ro" \
+    -v "$(pwd)/jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py:ro" \
+    -w "/home/jovyan/notebooks" \
+    -p 8888:8888 \
+    --network ${DOCKER_NETWORK_NAME} \
+    -e MYSQL_CONTAINER_NAME=${MYSQL_CONTAINER_NAME} \
+    ${FF_CONTAINER_NAME} jupyter notebook
