88fb60638079d7bbc8aa2224d14807805f98fe5c
Micah Smith <micahjsmith@gmail.com>
2018-03-15 14:14:48 -0400

    Refactor setup.py

--- src/setup.py
+++ src/setup.py
@@ -8,6 +8,31 @@ from setuptools import setup, find_packages
 name = "featurehub"
 pkg_name = name       
 
+requirements = ['dill',
+                'flask',
+                'jupyter',
+                'jupyterhub',
+                'matplotlib',
+                'numpy',
+                'mysqlclient',
+                'mysql-connector-python-rf',
+                'pandas',
+                'scikit-learn',
+                'scipy',
+                'sqlalchemy',
+                'sqlalchemy_utils',
+                'xxhash',
+               ]
+
+extras_require = {
+    'AutoSklearn': ['auto-sklearn==0.2.0'],
+}
+  
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
 with open(os.path.join(pkg_name, '__init__.py')) as f:
     for line in f:
         if line.startswith('__version__'):
@@ -23,20 +48,9 @@ setup(
     long_description = '',
     url              = 'https://github.com/HDI-Project/FeatureHub',
     packages         = find_packages(exclude=["tests", "__pycache__"]),
-    install_requires = [
-        'dill',
-        'flask',
-        'jupyter',
-        'jupyterhub',
-        'matplotlib',
-        'numpy',
-        'mysqlclient',
-        'mysql-connector-python-rf',
-        'pandas',
-        'scikit-learn',
-        'scipy',
-        'sqlalchemy',
-        'sqlalchemy_utils',
-        'xxhash',
-    ],
+    install_requires = requirements,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    extras_require   = extras_require,
  )
